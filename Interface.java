ed2ad187e59e0fd9a6e0e8e89df9f7164f930946
Anjana <anjanaithikkad4277@gmail.com>
2025-04-23 14:57:07 +0530

    Uompleted

--- Interface.java
+++ Interface.java
@@ -44,3 +44,37 @@ ksb@ksb-H410M-S2-V2:~/Anj7/OOps$ javac Interface.java
 (base) ksb@ksb-H410M-S2-V2:~/Anj7/OOps$ java Interface
 Academic Score: 85
 Sports Score: 90
+
+
+
+
+
+
+
+    Start
+
+   1. Define Interfaces:
+
+        Create interface Student with method showAcademicScore().
+
+        Create interface Sports with method showSportsScore().
+
+   2. Create Class Result:
+
+        Implement both interfaces (Student and Sports).
+
+        Declare variables academicScore and sportsScore with initial values.
+
+        Implement the method showAcademicScore() to print the academic score.
+
+        Implement the method showSportsScore() to print the sports score.
+
+        Implement the method displayResult() to print both academic and sports scores.
+
+   3. In the Main Method:
+
+        Create an instance of the Result class.
+
+        Call displayResult() to display both the academic and sports scores.
+
+End
